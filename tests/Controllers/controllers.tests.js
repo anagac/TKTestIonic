cb6f27e23a79b90486dc564aad74530f7865d875
Andres Aguilar <aaguilar@softstackfactory.com>
2015-07-15 17:52:32 +0000

    modified tests

--- tests/Controllers/controllers.tests.js
+++ tests/Controllers/controllers.tests.js
@@ -1,6 +1,6 @@
 describe('Controllers', function(){
     var scope;
-
+    var loginController;
     // load the controller's module
     beforeEach(module('ionic'));
     beforeEach(module('starter.controllers'));
@@ -9,10 +9,16 @@ describe('Controllers', function(){
     beforeEach(inject(function($rootScope, $controller) {
         scope = $rootScope.$new();
         $controller('LoginCtrl', {$scope: scope});
+        
     }));
 
     // tests start here
     it('should have a title with value of Login', function(){
         expect(scope.title).toEqual("Login");
     });
+    
+    it('should exists', function(){
+        expect(loginController).toBeDefined();
+    });
+    
 });
